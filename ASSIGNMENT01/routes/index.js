59ef5e592d3622085a1324a3c0a9509d588ce026
Pipetr <faaa1901@gmail.com>
2025-06-07 23:09:29 -0400

    routes to the different views created

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Felipe\'s Portfolio' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Felipe\'s Portfolio' });
+});
+
+/* GET About me page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
